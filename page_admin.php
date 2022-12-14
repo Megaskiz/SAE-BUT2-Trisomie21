18f996a0744c6f843005a63c3ca1cddef7ba28ed
jonathan.bzr <jonathan.bezara@icloud.com>
2022-12-14 17:39:22 +0100

    modif du php et css

--- page_admin.php
+++ page_admin.php
@@ -334,7 +334,7 @@ if (isset($_GET['id_suppr'])) {
                     echo "</section>";
                     
                     echo "<section class=\"nb-systeme\">";
-                    echo '<a href="page_creatsystem.php"><button class="acceder">creer un nouveau systeme</button></a>';
+                    echo '<a href="page_creatsystem.php"><button class="button_acceder">creer un nouveau systeme</button></a>';
 
 
                     //echo '<a href="page_creatsystem.php"><button class="acceder">creer un nouveau systeme</button></a>';
