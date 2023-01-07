709fc6cb86c8fb4f03916311fc4a4bd900e6d3a2
paul trochel <trochel.paul@outlook.fr>
2023-01-07 12:07:31 +0100

    explication du delete qui ne fontionne plus

--- suppr_sys.php
+++ suppr_sys.php
@@ -20,6 +20,7 @@ $req_suppr = "DELETE FROM message where id_objectif=$id_sys;DELETE FROM lier whe
 
 try {
     $res = $linkpdo->query($req_suppr);
+    // ne fonctionne plus parce qu'il y a le id sys dans la table "placer_jeton", il faut rajouter un champ " visible" dans la table objectif, pour qu'on l'affiche ou non
     header('Location: page_admin.php?id='.$_SESSION["id_enfant"]);
 } catch (Exception $e) { // toujours faire un test de retour au cas ou ça crash
     die('Erreur : ' . $e->getMessage());
