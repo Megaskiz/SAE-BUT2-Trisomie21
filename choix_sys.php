f3f9ea66a268fa62299b45b03b6431e62c9f6b58
paul trochel <trochel.paul@outlook.fr>
2023-01-12 10:47:27 +0100

    ajout des sessions dans la bd

--- choix_sys.php
+++ choix_sys.php
@@ -95,6 +95,166 @@ is_validateur();
         switch ($double_tab[0][10]) {
             case '1': {
 
+                // sys chargement
+
+                // si la session est obsolete, je change le nom du sys, j'uptate pour tout remettre à 0
+
+                //**************** */
+                try {
+                    // je recup la derniere session pour ce sys
+                    $session_max_query =$linkpdo->query("SELECT max(id_session) from placer_jeton where id_objectif=".$id);
+                    //$session_max_query->debugDumpParams();
+                    
+                    }
+                    catch (Exception $e) { // toujours faire un test de retour en cas de crash
+                        die('Erreur : ' . $e->getMessage());
+                    }
+            
+                    ///Affichage des entrées du résultat une à une
+                    
+                    $double_tab = $session_max_query -> fetchAll(); // je met le result de ma query dans un double tableau
+                    $session_max = $double_tab[0][0];
+            
+                    if($session_max==NULL){
+                        $session_max=0;     
+                    }
+            
+                try {
+                    //je recupere la date du premier jeton placé pour cette session dans ce sys
+                    $jeton_premier_query =$linkpdo->query("SELECT min(date_heure) from placer_jeton where id_session=".$session_max." and id_objectif=".$id);
+                    // je recupere la duree totale du sys prevu
+                    $duree_sys_query =$linkpdo->query("SELECT duree from OBJECTIF where id_objectif=".$id);
+               
+                    }
+                    catch (Exception $e) { // toujours faire un test de retour en cas de crash
+                        die('Erreur : ' . $e->getMessage());
+                    }
+            
+                    ///Affichage des entrées du résultat une à une
+                    
+                    $double_tab = $jeton_premier_query -> fetchAll();
+                    $double_tab2 = $duree_sys_query -> fetchAll();
+            
+                    $jeton_premier =  $double_tab[0][0];
+                    $duree_sys =  $double_tab2[0][0];
+            
+                    $duree_sys_en_seconde=$duree_sys*3600;
+            
+                    if($jeton_premier==NULL){
+                        $jeton_premier=0;
+                    }
+            
+                    // echo$session_max;
+                    // echo$jeton_max;
+            
+                    if($session_max ==0){
+                        // c'est le premier jetons qu'on place ever
+                        $session_actuelle=1;
+                        //inserer un jeton 'factice' dans la bd pour avoir la date de début de session
+                        $req = $linkpdo->prepare('insert into placer_jeton values ( :id_objectif, :time, :id_membre, :session)  ');
+
+
+                        if ($req == false){
+                            die("erreur linkpdo");
+                        }   
+                        try{
+                            $req->execute(array('id_objectif' => $id, 'time' => date("Y/m/d H:i:s"), 'id_membre' => $_SESSION['logged_user'], 'session' => $session_actuelle));
+                    
+                            if ($req == false){
+                                $req->debugDumpParams;
+                                die("erreur execute");
+                            }
+                        }
+                        catch (Exception $e){die('Erreur : ' . $e->getMessage());}
+
+
+                    }else{
+                        $secondes_premier_jeton=strtotime($jeton_premier);
+                        
+                        if($secondes_premier_jeton+$duree_sys_en_seconde<time()){
+
+                            $session_actuelle=$session_max+1;
+
+                            echo"date du premier jeton de la derniere session  : ".$jeton_premier;
+                            echo"<br>";
+
+                            echo($secondes_premier_jeton+$duree_sys_en_seconde);
+
+                            echo"<br>";
+
+                            echo(time());                            echo"<br>";
+
+
+                            echo($secondes_premier_jeton+$duree_sys_en_seconde-time());
+
+                            echo"<br>";
+
+                            echo"la duree du sys est : ".$duree_sys." heure(s)";
+
+                            echo"<br>";
+
+
+                            echo"depassement";
+
+                            //session sait quand on la dépasse, mais on ne crée pas un nouvelle session 
+
+                            //    A FAIRE 
+
+                            // update la date du jeton factice avec la date actuelle
+                            try {
+                                $req5 = $linkpdo->prepare('UPDATE placer_jeton SET date_heure = :nouvelle_heure, id_session= :id_session where date_heure = :ancienne_date ');
+                                // le nom est la chaine que j'utilise pour construire le système
+                                $req5->execute(array('nouvelle_heure' => date("Y/m/d H:i:s"),'id_session'=>$session_actuelle, 'ancienne_date' => $jeton_premier,));
+            
+                                //$req5 -> debugDumpParams();
+                            } catch (Exception $e) { // toujours faire un test de retour au cas ou ça crash
+                                die('Erreur : ' . $e->getMessage());
+                            }
+
+
+                            try {
+                                $res = $linkpdo->query("SELECT nom FROM objectif where id_objectif=$id"); // le nom est la chaine que j'utilise pour construire le système
+                            } catch (Exception $e) { // toujours faire un test de retour au cas ou ça crash
+                                die('Erreur : ' . $e->getMessage());
+                            }
+            
+                            ///Affichage des entrées du résultat une à une
+            
+                            $double_tab = $res->fetchAll();
+            
+                            $chaine = $double_tab[0][0]; // je recup ma chaine 'nom' du sys
+            
+                            $MaVariable = str_replace("1", "0", $chaine); // je la reset
+            
+                            try {
+                                $req5 = $linkpdo->prepare('UPDATE objectif SET nom = :intit where id_objectif = :id ');
+                                // le nom est la chaine que j'utilise pour construire le système
+                                $req5->execute(array('intit' => $MaVariable, 'id' => $id,));
+            
+                                //$req5 -> debugDumpParams();
+                            } catch (Exception $e) { // toujours faire un test de retour au cas ou ça crash
+                                die('Erreur : ' . $e->getMessage());
+                            }
+                        
+                        }
+                        else{
+                            echo($secondes_premier_jeton+$duree_sys_en_seconde-time());
+
+                            echo"<br>";
+                        }
+
+
+                //**************** */
+
+
+
+                
+                    
+                    
+                }
+
+                //
+
 
 
                     try {
@@ -103,6 +263,7 @@ is_validateur();
                     } catch (Exception $e) { // toujours faire un test de retour au cas ou ça crash
                         die('Erreur : ' . $e->getMessage());
                     }
+
 
                     ///Affichage des entrées du résultat une à une
 
@@ -118,6 +279,7 @@ is_validateur();
                     //echo$_POST['chaine'];
 
                     $chaine = $valeur[0];
+                    
 
                     // TESTER SI IL Y A DES 0 DANS LA CHAINE, SI NON, çA VEUT DIRE QUE LE SYSTEME EST FINI
                     if (strpos($chaine, 0) == false) {
@@ -135,8 +297,10 @@ is_validateur();
 
                     $morceau = explode(":", $chaine);
 
+
                     array_pop($morceau); // je retire la partie apres le dernier ":" 
                     $compteur = 0;
+
                     foreach ($morceau as $ligne) {
 
 
@@ -181,6 +345,7 @@ is_validateur();
                 break;
 
             case '3': {
+                // systeme tous les jours
 
 
                     try {
--- choix_sys_ajout.php
+++ choix_sys_ajout.php
@@ -46,13 +46,70 @@ if (isset ($_GET['case']))  {
     echo"<br>";
     echo"onvaenvoyerça: $tableau_final";
     exit();*/
+
+
+    // FAIRE EN SORTE DE RECUP LA DERNIERE SESSION QUI A ETE UTILISE POUR SE SYSTEME
+    // AJOUTER 1 A CE CHIFFRE SI LA DUREE DU SYSTEME DEPASSE LA DATE DU PREMIER JETON PLACE + LA DUREE DU SYS
+
+    try {
+        $session_max_query =$linkpdo->query("SELECT max(id_session) from placer_jeton where id_objectif=".$id);
+        //$session_max_query->debugDumpParams();
+        
+        }
+        catch (Exception $e) { // toujours faire un test de retour en cas de crash
+            die('Erreur : ' . $e->getMessage());
+        }
+
+        ///Affichage des entrées du résultat une à une
+        
+        $double_tab = $session_max_query -> fetchAll(); // je met le result de ma query dans un double tableau
+        $session_max = $double_tab[0][0];
+
+        if($session_max==NULL){
+            $session_max=0;     
+        }
+
+    try {
+        $jeton_max_query =$linkpdo->query("SELECT max(date_heure) from placer_jeton where id_session=".$session_max." and id_objectif=".$id);
+   
+        }
+        catch (Exception $e) { // toujours faire un test de retour en cas de crash
+            die('Erreur : ' . $e->getMessage());
+        }
+
+        ///Affichage des entrées du résultat une à une
+        
+        $double_tab = $jeton_max_query -> fetchAll();
+
+        $jeton_max =  $double_tab[0][0];
+
+        if($jeton_max==NULL){
+            $jeton_max=0;
+        }
+
+        echo$session_max;
+        echo$jeton_max;
+
+        if($session_max ==0){
+            $session_actuelle=1;
+        }else{
+            echo$jeton_max;
+            $secondes_dernier_jeton=strtotime($jeton_max);
+            echo$secondes_dernier_jeton;
+            // exit();
+            // $jeton_max
+            $session_actuelle=$session_max; // je garde la même session 
+            
+        }
+
+        
     
     // connexion bd 
 
     // modif dans le nom
 
     $req = $linkpdo->prepare('UPDATE objectif SET nom = :intit where id_objectif = :id ');
-    $req2 = $linkpdo->prepare('insert into placer_jeton values ( :id_objectif, :time, :id_membre)  ');
+    $req2 = $linkpdo->prepare('insert into placer_jeton values ( :id_objectif, :time, :id_membre, :session)  ');
 
     if ($req == false){
         die("erreur linkpdo");
@@ -61,7 +118,7 @@ if (isset ($_GET['case']))  {
     try{
         
         $req->execute(array('intit' => $tableau_final, 'id' => $id,));
-        $req2->execute(array('id_objectif' => $id, 'time' => date("Y/m/d H:i:s"), 'id_membre' => $_SESSION['logged_user']));
+        $req2->execute(array('id_objectif' => $id, 'time' => date("Y/m/d H:i:s"), 'id_membre' => $_SESSION['logged_user'], 'session' => $session_actuelle));
         header("Location:choix_sys.php?id_sys=$id");
 
 
