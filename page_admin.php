cdc73e9b420fa4f0b7e266342cb52e97aafb1d97
Adam Mende <*adammende7@gmail.com>
2023-01-14 18:37:53 +0100

    modif

--- page_admin.php
+++ page_admin.php
@@ -489,7 +489,6 @@ if (isset($_GET['eject'])) {
                             echo "</td>";
 
 
-<<<<<<< HEAD
                             #affiche message
                             echo "<td>";
                             echo"<center>";
@@ -595,115 +594,6 @@ if (isset($_GET['eject'])) {
                                     </form>
                                 </div>
                             </div>
-=======
-                             #affiche message
-                             echo "<td>";
-                             echo"<center>";
-                             echo "<button class=\"supprimer-objectif\" type=\"button\" onclick=\"openDialog('dialog_message" . $double_tab[$i][5] . "', this)\"> <span class=\" icon-mail\"> &#x2709; </span></button>";
-                             echo"</center>";
-                             echo "<div id=\"dialog_layer\" class=\"dialogs\">";
-                             echo "<div role=\"dialog\" id=\"dialog_message" . $double_tab[$i][5] . "\" aria-labelledby=\"dialog1_label\" aria-modal=\"true\" class=\"hidden\">";
-                             echo "<div class=\"dialog_form_actions3\">";
-                             echo "<button class=\"deco\" onclick=\"closeDialog(this)\">Retour</button>";
-                             echo "</div>";
-                             if (isset($double_tab[$i][5]) and !empty($double_tab[$i][5])) {
- 
-                                 /*$getid = $_GET['id_objectif'];
-                                     /*$recupUser = $linkpdo->prepare('SELECT * FROM membre where id_membre = ?');
-                                     $recupUser->execute(array($getid));
-                                     if($recupUser->rowCount() > 0){*/
-                                 if (isset($_POST["envoie".$double_tab[$i][5]])) {
-                                     $message = htmlspecialchars($_POST['messages']);
-                                     $sujet = htmlspecialchars($_POST['sujet']);
-                                     $insererMessage = $linkpdo->prepare('INSERT into message(corps,sujet,id_membre,date_heure,id_objectif) VALUES(?, ?, ?, NOW(), ?)');
-                                     if (!$insererMessage) {
-                                         die("Erreur prepare");
-                                     }
-                                     echo $double_tab[$i][5];
-                                     $insererMessage->execute(array($message, $sujet, $_SESSION['logged_user'], $double_tab[$i][5]));
-                                     
-                                     if (!$insererMessage) {
-                                         die("Erreur execute");
-                                     }
-                                 }
-                                 /*}else{
-                                         echo ("aucun utilisateur trouvé");
-                                     }*/
-                             } else {
-                                 echo ("aucun id trouvé");
-                             }
-                             echo "<title>Envoie de mesage</title>";
- 
- 
-                             
- 
-                 ?>
-                             <div class="chat_all">
-                                 <div class="chat_title">
-                                         <svg class="chat_svg" aria-hidden="true" data-prefix="fas" data-icon="comment-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
-                                         <path fill="currentColor" d="M448 0H64C28.7 0 0 28.7 0 64v288c0 35.3 28.7 64 64 64h96v84c0 9.8 11.2 15.5 19.1 9.7L304 416h144c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64z"></path>
-                                     </svg>
-                                     Messagerie du système à jeton
-                                 </div>
-                                 <div class="chat_list_msg">
-                                     <section id="message">
-                                         <?php
-                                         $recupMessages = $linkpdo->prepare('SELECT sujet,corps,date_heure,membre.id_membre, membre.nom, membre.prenom FROM message,membre WHERE id_objectif = ? and membre.id_membre = message.id_membre');
-                                         if (!$recupMessages) {
-                                             die("Erreur prepare");
-                                         }
-                                         $recupMessages->execute(array($double_tab[$i][5]));
-                                         if (!$recupMessages) {
-                                             die("Erreur prepare");
-                                         }
- 
-                                         while ($message = $recupMessages->fetch()) {
-                                             if ($message['id_membre'] == $_SESSION['logged_user']) {
-                                         ?>
-                                                 <div class="chat_msgR">
-                                                     <img class="chat_img_R" src="/sae-but2-s1/img/user_logo.png" alt="tete de l'utilisateur">
-                                                     <div class="chat_vous">
-                                                         <div class="chat_info">
-                                                             <div class="chat_nomm"><?= ucfirst($message["nom"] . " " . $message["prenom"] . " (vous) : ") ?></div>
-                                                             <div class="chat_datem"><?= "le " . (new DateTime($message["date_heure"]))->format("d/m/Y H\hi") ?></div>
-                                                         </div>
-                                                         <p class="chat_zone_txt"> <?= "Sujet :" . $message["sujet"] . "<br>" . $message["corps"]; ?> </p>
-                                                     </div>
-                                                 </div>
-                                             <?php
-                                             } else {
-                                             ?>
-                                                 <div class="chat_msgL">
-                                                     <img class="chat_img_L" src="/sae-but2-s1/img/user_logo.png" alt="tête de l'utilisateur">
-                                                     <div class="chat_autre">
-                                                         <div class="chat_info">
-                                                             <div class="chat_nomm"><?= ucfirst($message["nom"] . " " . $message["prenom"]) ?></div>
-                                                             <div class="chat_datem"><?= "le " . (new DateTime($message["date_heure"]))->format("d/m/Y H\hi") ?></div>
-                                                         </div>
-                                                         <p class="chat_zone_txt"> <?= "Sujet :" . $message["sujet"] . "<br>" . $message["corps"]; ?> </p>
-                                                     </div>
-                                                 </div>
-                                             <?php
-                                             }
-                                             ?>
-                                         <?php
-                                         }
-                                         ?>
-                                     </section>
-                                 </div>
-                                 <div class="chat_envoi_msg">
-                                     <form method="POST" action="" class="">
-                                         <div class="chat_sujet_msg">
-                                             <input type="text" id="sujet" name="sujet" class="chat_sujet" placeholder="Sujet ..." required></br>
-                                         </div>
-                                         <div class="chat_txt_msg">
-                                             <input class="chat_messages" name="messages" placeholder="Entrez votre message ..." required></br>
-                                             <button type="submit" class="chat_send" name=<?="envoie".$double_tab[$i][5]?>>Envoyer</button>
-                                         </div>
-                                     </form>
-                                 </div>
-                             </div>
->>>>>>> 9920b617b81771791e5a9df473242e2f2f7f7c9c
                             
                     <?php
 
