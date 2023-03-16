c8d39b7f1930aaa21602a4c695128c915378e70b
paul trochel <trochel.paul@outlook.fr>
2023-03-16 15:30:20 +0100

    modif test.php, encore a changer

--- test.php
+++ test.php
@@ -3,23 +3,20 @@ session_start();
 if (isset($_POST["radio1"]) & isset($_POST["radio2"])) {
     $res1 = $_POST["radio1"];
     $res2 = $_POST["radio2"];
-    echo"choix de système : ".$res1."<br>";
-    echo"choix de récompense : ".$res2."<br>";
-    echo"identifiant de l'enfant : ".$_SESSION['id_enfant']."<br>";
-    echo"identifiant de l'enfant : ".$_SESSION['logged_user']."<br>";
-   // je met le type de système et de recompenses dans une variable superglobale pour que je récupère les informations dans les pages suivantes sans faire de post, ou de get
+    // echo"choix de système : ".$res1."<br>";
+    // echo"choix de récompense : ".$res2."<br>";
+    // echo"identifiant de l'enfant : ".$_SESSION['id_enfant']."<br>";
+    // echo"identifiant de l'enfant : ".$_SESSION['logged_user']."<br>";
+   
+    // je met le type de système et de recompenses dans une variable superglobale pour que je récupère les informations dans les pages suivantes sans faire de post, ou de get
     $_SESSION["type_sys"]= $res1;
     $_SESSION["type_rec"]=$res2;
 
-    switch ($_SESSION["type_sys"]) {
+    switch ($_SESSION["type_sys"]) { // en fonction du type d'objectif choisi, on aiguille vers la bonne page
         case '1':
             header('Location: creation_systeme_chargement.php');
             break;
-        
-        case '2':
-            echo"page pas encore creée";
-            break;
-
+    
         case '3':
             header('Location: creation_systeme_contrat.php');
             break;
