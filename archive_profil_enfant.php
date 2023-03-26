ba3dcedd8e1c68a6eb4f216b79ac8f3b204ea8f6
paul.trochel <trochel.paul@outlook.fr>
2023-03-26 17:07:05 +0200

    ménage dans le fichier

--- archive_profil_enfant.php
+++ archive_profil_enfant.php
@@ -27,7 +27,6 @@ $linkpdo = connexionBd();
                 <a class="retour" href="index.php"> Retour</a>
             </div>
             <?php
-            
             if ($_SESSION["role_user"] != 2) {
                 try {
                     //acces tous les enfants
@@ -38,26 +37,23 @@ $linkpdo = connexionBd();
                 } catch (Exception $e) { 
                     die('Erreur : ' . $e->getMessage());
                 }
-
                 $double_tab = $res->fetchAll(); 
                 $nombre_ligne = $res->rowCount();
                 $liste = array();
 
                 echo "
                 <div class='liste-enfant'>
-                <div class=\"recherche\">
-                <form class='recherche' method=\"post\" action=\"search.php\">
-                <div>
-                <input class=\"input_recherche\" type=\"text\" placeholder=\"Mots-clés ...\" id=\"keywords\" name=\"keywords\" required> 
-                </div>
-                <input class=\"bouton_recherche\" type=\"submit\" value=\" &#x1F50E;\">
-                </form>
-                </div>
+                    <div class=\"recherche\">
+                        <form class='recherche' method=\"post\" action=\"search.php\">
+                            <div>
+                                <input class=\"input_recherche\" type=\"text\" placeholder=\"Mots-clés ...\" id=\"keywords\" name=\"keywords\" required> 
+                            </div>
+                            <input class=\"bouton_recherche\" type=\"submit\" value=\" &#x1F50E;\">
+                        </form>
+                    </div>
                 <table >
                 ";
-
                 for ($i = 0; $i < $nombre_ligne; $i++) {
-
                     for ($y = 1; $y < 3; $y++) {
                         echo "<td>";
                         print_r(ucfirst(htmlspecialchars($double_tab[$i][$y])));
@@ -67,32 +63,22 @@ $linkpdo = connexionBd();
                         $age = $double_tab[0][$y];
                         echo "</td>";
                     }
-
                     $identifiant = $double_tab[$i][0];
                     echo "<td>";
                     echo '<a href="archive_profil_enfant.php?id=' . $identifiant . '"><button  class="acceder-information-enfant">Acceder</button> </a>';
                     echo "</td>";
                     echo "</tr>";
                 }
-
                 echo "</table>";
-
-                
                 $res->closeCursor();
             }
             ?>
             </div>
         </nav>
-
         <?php 
-
         if (isset($_GET['id'])) {
             $id = $_GET['id'];
             $_SESSION["id_enfant"] = $id;
-
-
-
-            
             try {
                 $res = $linkpdo->query("SELECT * FROM enfant where id_enfant='$id'");
             } catch (Exception $e) { 
@@ -102,7 +88,6 @@ $linkpdo = connexionBd();
             $double_tab = $res->fetchAll(); 
             $nombre_ligne = $res->rowCount(); 
             $liste = array();
-
 
             $id_enfant = $double_tab[0][0];
             $nom_enfant = ucfirst($double_tab[0][1]);
@@ -135,52 +120,46 @@ $linkpdo = connexionBd();
                 <?php
                 if (isset($_GET['id'])) {
                     $_SESSION['id_enfant'] = $_GET['id'];
-
-
                     //<!---- menu droit information sur l'enfant ---->
-                    echo "<div class=\"div-photo-enfant\">";
-                    echo "<img class=\"photo-enfant\" src=\"" . htmlspecialchars($photo_enfant) . "\" alt=\"photo du visage de " . htmlspecialchars($prenom_enfant) . "\">";
-                    echo "</div>";
-
-
-
-                    echo "<div class=\"case-3-infos\">";
-                    echo "<p class=\"info\">  Nom :<strong> " . htmlspecialchars($nom_enfant) . "</strong></p>";
-                    echo "<p class=\"info\">Date de Naissance :<strong>  " . htmlspecialchars($ddn_enfant) . " </strong></p>";
-                    echo "<p class=\"info\">Activité enfant :<strong>  " . htmlspecialchars($activite) . "    </strong></p>";
-                    echo "</div>";
-
-                    echo "<div class=\"case-3-infos\">";
-                    echo "<p class=\"info\">Prénom : <strong> " . htmlspecialchars($prenom_enfant) . "  </strong></p>";
-                    echo "<p class=\"info\">Adresse enfant : <strong>  " . htmlspecialchars($adresse) . "    </strong> </p>";
-                    echo "<p class=\"info\">Handicap enfant :<strong>  " . htmlspecialchars($handicap) . "     </strong></p>";
-                    echo "</div>";
-
-
-                    echo " <div class=\"div-modif-enfant\">";
-                    echo "</div>";
-
-                    echo "<center>";
-                    echo '<button class="button_ajouter-objectif" type="button" onclick="openDialog(\'dialog6\', this)">Dé-archiver ce profil</button>';
-                    echo '<div role="dialog" id="dialog6" aria-labelledby="dialog1_label" aria-modal="true" class="hidden">';
-                    echo "<p class='popup-txt' > Voulez-vous restaurer le compte de cet enfant ? Il sera visible par toutes les personnes l'ayant suivi </p>";
-                    echo '<div style="display:flex; justify-content: space-evenly;">';
-                    echo '<button class="popup-btn" type="button" onclick="closeDialog(this)">Annuler</button>';
-                    echo '<a  class="popup-btn"  href="appel_fonction.php?appel=restaure_profil_enfant&id='.$_GET["id"].'">Valider</a>';
-                    echo "</div>";
-                    echo "</div>";
-                    echo '<button class="button_ajouter-objectif" type="button" onclick="openDialog(\'dialog7\', this)">Supprimer ce profil</button>';
-                    echo '<div role="dialog" id="dialog7" aria-labelledby="dialog1_label" aria-modal="true" class="hidden">';
-                    echo "<p class='popup-txt' > Voulez-vous supprimer le compte de cet enfant ? Cette action est définitive, cela supprimera tous ses objetifs, tous les messages associés, toutes les récompenses, et les statistiques de cet enfant. </p>";
-                    echo '<div style="display:flex; justify-content: space-evenly;">';
-                    echo '<button class="popup-btn" type="button" onclick="closeDialog(this)">Annuler</button>';
-                    echo '<a  class="popup-btn"  href="appel_fonction.php?appel=supprime_profil_enfant&id_enfant=' . $_GET["id"] . '">Valider</a>';
-                    echo "</div>";
-                    echo "</div>";
-                    echo "</center>";
-                    echo "</div>";
-                    echo "</section>";
-                    echo "<section class=\"nb-systeme\">";
+                    echo "
+                    <div class=\"div-photo-enfant\">
+                        <img class=\"photo-enfant\" src=\"" . htmlspecialchars($photo_enfant) . "\" alt=\"photo du visage de " . htmlspecialchars($prenom_enfant) . "\">
+                    </div>
+                    <div class=\"case-3-infos\">
+                        <p class=\"info\">  Nom :<strong> " . htmlspecialchars($nom_enfant) . "</strong></p>
+                        <p class=\"info\">Date de Naissance :<strong>  " . htmlspecialchars($ddn_enfant) . " </strong></p>
+                        <p class=\"info\">Activité enfant :<strong>  " . htmlspecialchars($activite) . "    </strong></p>
+                    </div>
+                    <div class=\"case-3-infos\">
+                        <p class=\"info\">Prénom : <strong> " . htmlspecialchars($prenom_enfant) . "  </strong></p>
+                        <p class=\"info\">Adresse enfant : <strong>  " . htmlspecialchars($adresse) . "    </strong> </p>
+                        <p class=\"info\">Handicap enfant :<strong>  " . htmlspecialchars($handicap) . "     </strong></p>
+                    </div>
+                        <div class=\"div-modif-enfant\">
+                    </div>
+                    <center>
+                        <button class=\"button_ajouter-objectif\" type=\"button\" onclick=\"openDialog('dialog6', this)\">Dé-archiver ce profil</button>
+                        <div role=\"dialog\" id=\"dialog6\" aria-labelledby=\"dialog1_label\" aria-modal=\"true\" class=\"hidden\">
+                            <p class='popup-txt' > Voulez-vous restaurer le compte de cet enfant ? Il sera visible par toutes les personnes l'ayant suivi </p>
+                            <div style=\"display:flex; justify-content: space-evenly;\">
+                                <button class=\"popup-btn\" type=\"button\" onclick=\"closeDialog(this)\">Annuler</button>
+                                <a  class=\"popup-btn\"  href=\"appel_fonction.php?appel=restaure_profil_enfant&id=".$_GET['id']."\">Valider</a>
+                            </div>
+                        </div>
+    
+                        <button class=\"button_ajouter-objectif\" type=\"button\" onclick=\"openDialog('dialog7', this)\">Supprimer ce profil</button>
+                        <div role=\"dialog\" id=\"dialog7\" aria-labelledby=\"dialog1_label\" aria-modal=\"true\" class=\"hidden\">
+                            <p class='popup-txt' > Voulez-vous supprimer le compte de cet enfant ? Cette action est définitive, cela supprimera tous ses objetifs, tous les messagesssociés, toutes les récompenses, et les statistiques de cet enfant. </p>
+                            <div style=\"display:flex; justify-content: space-evenly;\">
+                                <button class=\"popup-btn\" type=\"button\" onclick=\"closeDialog(this)\">Annuler</button>
+                                <a  class=\"popup-btn\"  href=\"appel_fonction.php?appel=supprime_profil_enfant&id_enfant=".$_GET["id"]."\">Valider</a>
+                            </div>
+                        </div>
+                    </center>
+                    
+                </div>
+            </section>
+            <section class=\"nb-systeme\">";
 
                     // tous les systèmes de l'enfant :
                     try {
@@ -264,10 +243,13 @@ $linkpdo = connexionBd();
                             }
                             echo "</td>";
                             #affiche message
-                            echo "<td class='sms'>";
-                            echo "<center>";
-                            echo "<button class=\"\" type=\"button\" onclick=\"', this)\"> <span class=\" icon-mail\"> Messagerie &#x2709; </span></button>";
-                            echo "</center>";
+                            echo "
+                            <td class='sms'>
+                                <center>
+                                    <button class=\"\" type=\"button\" onclick=\"', this)\"> <span class=\" icon-mail\"> Messagerie &#x2709; </span></button>
+                                </center>
+                            </td>
+                            ";
                             
                             #affiche status
                             echo "<td class='sms'>";
@@ -286,18 +268,20 @@ $linkpdo = connexionBd();
                             if ($_SESSION["role_user"] == 1 or $_SESSION["role_user"] == 3) {
                                 switch ($double_tab[$i][4]) {
                                     case 1:
-                                        echo "<td>";
-                                        echo "<center>";
-                                        echo '<a href=""><button class="status-objectif">Ne plus utiliser</button></a>';
-                                        echo "</center>";
-                                        echo "</td>";
+                                        echo "
+                                        <td>
+                                            <center>
+                                                <a href=\"\"><button class=\"status-objectif\">Ne plus utiliser</button></a>
+                                            </center>
+                                        </td>";
                                         break;
                                     case 0:
-                                        echo "<td>";
-                                        echo "<center>";
-                                        echo '<a href=""><button class="status-objectif">Commencer l\'utilisation</button></a>';
-                                        echo "</center>";
-                                        echo "</td>";
+                                        echo "
+                                        <td>
+                                            <center>
+                                                <a href=\"\"><button class=\"status-objectif\">Commencer l'utilisation</button></a>
+                                            </center>
+                                        </td>";
                                         break;
                                 }
                             }
@@ -310,22 +294,23 @@ $linkpdo = connexionBd();
                             echo " <div class=\"case-enfant\">";
                             //bouton supprimer un sys -> "archiver"
                             if ($_SESSION["role_user"] == 1) {
-                                echo "<center>";
-                                echo "<button class=\"supprimer-objectif\" type=\"button\" onclick=\"', this)\"><img class='delet-icon' src='img/delete.png'></a></button>";
-                                echo "</center>";
-                                echo "<div id=\"dialog_layer\" class=\"dialogs\">";
-                                echo "<div role=\"dialog\" id=\"dialog" . $double_tab[$i][5] . "\" aria-labelledby=\"dialog1_label\" aria-modal=\"true\" class=\"hidden\">";
-                                echo "<p> Attention, archiver ce système le retirera de tous les affichages et des statistiques, il ne sera accessible qu'aux coordinateur et à l'administrateur, dans l'archive</p>";
-                                echo "<div class=\"dialog_form_actions\">";
-                                echo "<a href=\"suppr_sys.php?id_sys=" . $double_tab[$i][5] . "\"><button class='sup-objectif'>Supprimer le système</button></a>";
-                                echo "<button class=\"deco\" onclick=\"closeDialog(this)\">Annuler</button>";
-                                echo "</div>";
+                                echo "
+                                <center>
+                                    <button class=\"supprimer-objectif\" type=\"button\" onclick=\"', this)\"><img class='delet-icon' src='img/delete.png'></a></button>
+                                </center>
+                                <div id=\"dialog_layer\" class=\"dialogs\">
+                                    <div role=\"dialog\" id=\"dialog" . $double_tab[$i][5] . "\" aria-labelledby=\"dialog1_label\" aria-modal=\"true\" class=\"hidden\">
+                                        <p> Attention, archiver ce système le retirera de tous les affichages et des statistiques, il ne sera accessible qu'aux coordinateur et à l'administrateur, dans l'archive</p>
+                                        <div class=\"dialog_form_actions\">
+                                            <a href=\"suppr_sys.php?id_sys=" . $double_tab[$i][5] . "\"><button class='sup-objectif'>Supprimer le système</button></a>
+                                            <button class=\"deco\" onclick=\"closeDialog(this)\">Annuler</button>
+                                        </div>";
                             }
-                            echo "</div>";
-                            echo "</div>";
-                            echo "</td>";
-
-                            echo "</tr>";
+                            echo "
+                            </div>
+                            </div>
+                            </td>
+                            </tr>";
                         }
                     }
                     echo "</table>";
